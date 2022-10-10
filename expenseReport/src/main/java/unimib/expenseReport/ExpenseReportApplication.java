6c1766a117ed22fdc69e27091e3f8f38805c9343
andrew-unimib <76683808+andrew-unimib@users.noreply.github.com>
2022-10-10 15:44:56 +0200

    Initial Configuration

---
+++ expenseReport/src/main/java/unimib/expenseReport/ExpenseReportApplication.java
@@ -0,0 +1,13 @@
+package unimib.expenseReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReportApplication.class, args);
+	}
+
+}
---
+++ expenseReport/src/test/java/unimib/expenseReport/ExpenseReportApplicationTests.java
@@ -0,0 +1,13 @@
+package unimib.expenseReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
